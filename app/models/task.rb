722a4ca4b3c607b7504aa253e7f97dc9872680dc
Juan Pedro Mermoz <jpmermoz@hotmail.com>
2014-06-25 11:37:52 -0300

    Destroy tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,26 +1,21 @@
 class Task
 
-	attr_accessor :user, :pid, :cpu, :mem, :vmz, :rss, :tty, :stat, :start, :time, :command
+	attr_accessor :user, :pid, :cpu, :mem, :vmz, :rss, :tty, :stat, :start, :time, :command, :errors
 
-	def initialize(cols)
-		@user = cols[0]
-		@pid = cols[1]
-		@cpu = cols[2]
-		@mem = cols[3]
-		@vmz = cols[4]
-		@rss = cols[5]
-		@tty = cols[6]
-		@stat = cols[7]
-		@start = cols[8]
-		@time = cols[9]
-		@command = cols[10, cols.size].join(' ')
+	def initialize(args = {})
+		args.each do |key, value|
+			self.instance_variable_set("@#{key}", value)
+		end
+
+		@errors = []
 	end
 
 	def self.all
 		tasks = []
 		`ps faxu --width 1000 --no-headers`.split("\n").each do |line|
-			tasks << Task.new(line.split(' '))
+			tasks << Task.construct(line.split(' '))
 		end
+
 		tasks
 	end
 
@@ -32,12 +27,28 @@ class Task
 		nil
 	end
 
-	def self.find_by_name(name)
-		all.each do |p|
-			return p if p.command.include?(name)
+	def save
+		begin
+			pid = Process.spawn("#{self.command}")
+			p = Task.find(pid)
+
+			self.user = p.user
+			self.pid = p.pid
+			self.cpu = p.cpu
+			self.mem = p.mem
+			self.vmz = p.vmz
+			self.rss = p.rss
+			self.tty = p.tty
+			self.stat = p.stat
+			self.start = p.start
+			self.time = p.time
+			self.command = p.command
+		rescue Exception => e
+			self.errors << e.message
+			return false
 		end
 
-		nil
+		true
 	end
 
 	def update(task_params)
@@ -51,7 +62,13 @@ class Task
 	end
 
 	def destroy
-		`kill "#{self.pid}"`
+		begin
+			Process.kill(:KILL, self.pid.to_i)
+		rescue Exception => e
+			self.errors << e.message
+			return false
+		end
+
 		true
 	end
 
@@ -70,5 +87,25 @@ class Task
     p[:command] = command
     p[:priority] = priority
     p
+	end
+
+	private
+
+	def self.construct(cols = [])
+		t = Task.new
+
+		t.user = cols[0]
+		t.pid = cols[1]
+		t.cpu = cols[2]
+		t.mem = cols[3]
+		t.vmz = cols[4]
+		t.rss = cols[5]
+		t.tty = cols[6]
+		t.stat = cols[7]
+		t.start = cols[8]
+		t.time = cols[9]
+		t.command = cols[10, cols.size].join(' ')
+
+		t
 	end
 end
